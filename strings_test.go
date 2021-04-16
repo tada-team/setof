9e8a479e3e4737b6f2de2eccbcbc170764b38a48
Maxim Oransky <maxim.oransky@gmail.com>
2021-04-16 20:38:16 +0300

    tests fix

--- strings_test.go
+++ strings_test.go
@@ -3,7 +3,7 @@ package setof
 import "testing"
 
 func TestSet(t *testing.T) {
-	s := Strings().Update("a").Update("a")
+	s := NewStrings().Update("a").Update("a")
 	if len(s.SortedList()) != 1 || s.SortedList()[0] != "a" {
 		t.Error("StringsSet fail")
 	}
